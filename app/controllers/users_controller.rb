7a30a8ca8e64f7d0da52d6ad72dced685da50f15
kumini88 <gkumini8874424362@yahoo.co.jp>
2017-10-24 02:14:08 +0000

    edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in, only: [:index]
+  before_action :correct_user, only: [:show]
   
   def index
     @users = User.all.page(params[:page])
@@ -33,5 +34,10 @@ class UsersController < ApplicationController
   
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to root_url if @user != current_user
   end
 end
